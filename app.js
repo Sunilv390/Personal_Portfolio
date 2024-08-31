87009af0f90dafd033dffc514be6c4249f6b6bea
Sunil Verma <sunilv390@gmail.com>
2024-08-31 18:39:32 +0530

    Added : Dark mode

--- app.js
+++ app.js
@@ -4,3 +4,29 @@ const toggleMenu = () => {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 };
+
+document.addEventListener("DOMContentLoaded", () => {
+  const modeToggle = document.getElementById("mode");
+  const body = document.body;
+
+  // Check local storage for theme preference
+  const savedTheme = localStorage.getItem("theme");
+  if (savedTheme) {
+    body.classList.toggle("dark-mode", savedTheme === "dark");
+    modeToggle.src =
+      savedTheme === "dark"
+        ? "./assets/dark-mode1.png"
+        : "./assets/light-mode1.png";
+  }
+
+  modeToggle.addEventListener("click", () => {
+    body.classList.toggle("dark-mode");
+    const isDarkMode = body.classList.contains("dark-mode");
+    modeToggle.src = isDarkMode
+      ? "./assets/dark-mode1.png"
+      : "./assets/light-mode1.png";
+
+    // Save theme preference to local storage
+    localStorage.setItem("theme", isDarkMode ? "dark" : "light");
+  });
+});
